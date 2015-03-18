f2fda53ba76f0659acde78abb32b21716543aad0
Marcelo Juchem <juchem@gmail.com>
2015-03-18 14:03:42 -0700

    smaller fixes for square_root

--- 18-square_root/square_root.h
+++ 18-square_root/square_root.h
@@ -6,14 +6,14 @@
 #include <cmath>
 
 template <typename T>
-T square_root(T n, T precision, unsigned &iterations) {
+T square_root(T n, T precision, std::size_t &iterations) {
 	T pivot = n;
 
 	iterations = 0;
 
 	for(T last = 0; precision < std::abs(pivot - last); ++iterations) {
 		last = pivot;
-		pivot = (pivot + (n / pivot)) / T(2);
+		pivot = (pivot + (n / pivot)) / 2;
 	}
 
 	return pivot;
